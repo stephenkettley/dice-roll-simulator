09d2c8bf200cb4e2609192fa16664530e308bbb2
Stephen Kettley <kettleystephen@gmail.com>
2023-09-01 11:12:46 +0200

    create js functionality

---
+++ index.js
@@ -0,0 +1,53 @@
+const buttonElement = document.getElementById("roll-button");
+const diceElement = document.getElementById("dice");
+const listElement = document.getElementById("roll-history");
+
+let historyList = [];
+
+function getDiceFace(rollNumber) {
+  switch (rollNumber) {
+    case 1:
+      return "&#9856";
+    case 2:
+      return "&#9857";
+    case 3:
+      return "&#9858";
+    case 4:
+      return "&#9859";
+    case 5:
+      return "&#9860";
+    case 6:
+      return "&#9861";
+    default:
+      "";
+  }
+}
+
+function selectNewDice() {
+  const rollNumber = Math.floor(Math.random() * 6) + 1;
+  const diceFace = getDiceFace(rollNumber);
+  diceElement.innerHTML = diceFace;
+  historyList.push(rollNumber);
+  updateRollHistory();
+}
+
+function updateRollHistory() {
+  listElement.innerHTML = "";
+  for (let i = 0; i < historyList.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(
+      historyList[i]
+    )}</span`;
+    listElement.appendChild(listItem);
+  }
+}
+
+function rollDice() {
+  diceElement.classList.add("roll-animation");
+  setTimeout(() => {
+    diceElement.classList.remove("roll-animation");
+    selectNewDice();
+  }, 1000);
+}
+
+buttonElement.addEventListener("click", rollDice);
